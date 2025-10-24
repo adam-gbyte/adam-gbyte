356f2291b3d15127b67dcfef25e7dd2ddc1936d0
Adam Gumilang <80865879+adam-gbyte@users.noreply.github.com>
2025-10-24 14:00:20 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -149,7 +149,7 @@
 
       <div class="flex justify-end">
         <button
-          class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          class="bg-blue-500 text-white cursor-pointer px-4 py-2 rounded-lg hover:bg-blue-600"
           on:click={closeModal}
         >
           Tutup
